b16a51de7cdd42ca17af3126d347daecba4caade
Dahyana Romero <105084597+RomeroDahyana@users.noreply.github.com>
2023-11-03 23:32:45 -0300

    Delete Proyecto final/admin.py

--- Proyecto final/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Ciudad, Estado, Operacion, Propiedad
-# Register your models here.
-
-admin.site.register(Ciudad)
-admin.site.register(Estado)
-admin.site.register(Operacion)
-admin.site.register(Propiedad)
